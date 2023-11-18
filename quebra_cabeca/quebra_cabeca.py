ef5bebeaa605f9c6a4eabd33ec293313741dfcbd
PedroPortow <pedrolportow@gmail.com>
2023-11-18 18:07:04 -0300

    busca em largura quase lá eu acho

--- quebra_cabeca/quebra_cabeca.py
+++ quebra_cabeca/quebra_cabeca.py
@@ -1,5 +1,5 @@
 from tabuleiro import Tabuleiro
-from collections import  deque
+from queue import Queue
 
 # Algoritmos necessários:
 
@@ -24,21 +24,51 @@ from collections import  deque
 class QuebraCabeca:
   def __init__(self, lado, random = True):
     self.lado = lado
+    self.last_position = lado * lado
     self.tabuleiro = Tabuleiro(lado, random)
     
   def busca_em_largura(self):
-    pass
+    # fila vai ter que armazenar os estados a serem explorados
+    # solução ótima
+    queue_to_visit = [self.tabuleiro]
+    visited_states = []
+
+    print(queue_to_visit)
+
+    while queue_to_visit:
+      board_current_state = queue_to_visit.pop(0)
+      # print(board_current_state.tabuleiro)
+
+      if self.final_state_verifier(board_current_state.tabuleiro):
+        return
+
+      visited_states.append(board_current_state)
+      # print(visited_states)
+
+      possible_board_states, possible_movements = board_current_state.movimentos_possiveis()
+      
+      for possible_board_states, possible_movements in zip(possible_board_states, possible_movements):
+        # new_board_state = 
+
+  def new_board_state(self, board):
+    pass     
   
-  def final_state_verifier(self):
-    counter = 0  
+  def final_state_verifier(self, board):
+    counter = 1 
     in_order_flag = True
+    
+    # verifica se tá em ordem crescente 1, 2, 3... e o ultimo é 0
 
     for i in range(self.lado):
         for j in range(self.lado):
-            print(counter)
-            if self.tabuleiro.tabuleiro[i][j] != counter:
+            if counter == self.last_position:
+              if board[i][j] != 0:
                 in_order_flag = False
                 break
+            elif board[i][j] != counter:
+                in_order_flag = False
+                break
+            
             counter += 1
         if not in_order_flag:
             break
@@ -46,9 +76,7 @@ class QuebraCabeca:
     print("Em ordem! (estado final)" if in_order_flag else "Não está em ordem (não tá no estado final)" )
     return in_order_flag
 
-
-            
 quebra_cabeca = QuebraCabeca(2)
 quebra_cabeca.tabuleiro.print_tabuleiro()
-quebra_cabeca.final_state_verifier()
-# solução = quebra_cabeca.busca_em_largura()
+# quebra_cabeca.final_state_verifier()
+quebra_cabeca.busca_em_largura()
