1db1f86a71bb57d7d72ea388d1e6fd9163640f9e
PedroPortow <pedrolportow@gmail.com>
2023-11-21 10:33:38 -0300

    bfs almost done

--- quebra_cabeca/quebra_cabeca.py
+++ quebra_cabeca/quebra_cabeca.py
@@ -1,5 +1,6 @@
 from tabuleiro import Tabuleiro
 from queue import Queue
+import copy
 
 # Algoritmos necessários:
 
@@ -26,39 +27,43 @@ class QuebraCabeca:
     self.lado = lado
     self.last_position = lado * lado
     self.tabuleiro = Tabuleiro(lado, random)
+    self.realized_movements = []
     
   def busca_em_largura(self):
-    # fila vai ter que armazenar os estados a serem explorados
-    # solução ótima
     queue_to_visit = [self.tabuleiro]
     visited_states = []
-
-    print(queue_to_visit)
-
+    
     while queue_to_visit:
-      board_current_state = queue_to_visit.pop(0)
-      # print(board_current_state.tabuleiro)
-
-      if self.final_state_verifier(board_current_state.tabuleiro):
-        return
-
-      visited_states.append(board_current_state)
-      # print(visited_states)
-
-      possible_board_states, possible_movements = board_current_state.movimentos_possiveis()
+      current_board_state = queue_to_visit.pop()
       
-      for possible_board_states, possible_movements in zip(possible_board_states, possible_movements):
-        # new_board_state = 
+      print("------------------------------")
+      current_board_state.print_tabuleiro()
+      print("------------------------------")
+      
+      if self.final_state_verifier(current_board_state.tabuleiro):
+        print("achou a solução")
+        return
+      
+      # transformar pra string e adicioanr no visited_states pra comparacao
+      current_state_to_str = str(current_board_state.tabuleiro) 
+      
+      possible_board_states, possible_movements = current_board_state.movimentos_possiveis()
+      
+      #pra cada movimento, deve adicionar em uma fila
+      for next_board_state, movement in zip(possible_board_states, possible_movements):
+        print(movement)
+        new_board = self.new_board_state(current_board_state, movement)
+        queue_to_visit.append(new_board)
 
-  def new_board_state(self, board):
-    pass     
-  
+  def new_board_state(self, current_board, movement):
+    new_board = Tabuleiro(self.lado, False, current_board.tabuleiro)
+    new_board.identificar_movimento(movement)
+    return new_board
+      
   def final_state_verifier(self, board):
     counter = 1 
     in_order_flag = True
     
-    # verifica se tá em ordem crescente 1, 2, 3... e o ultimo é 0
-
     for i in range(self.lado):
         for j in range(self.lado):
             if counter == self.last_position:
@@ -72,11 +77,15 @@ class QuebraCabeca:
             counter += 1
         if not in_order_flag:
             break
+        
+        if in_order_flag:
+           print("CABOBOBOBOBOO")
 
-    print("Em ordem! (estado final)" if in_order_flag else "Não está em ordem (não tá no estado final)" )
     return in_order_flag
 
 quebra_cabeca = QuebraCabeca(2)
 quebra_cabeca.tabuleiro.print_tabuleiro()
+# quebra_cabeca.tabuleiro.identificar_movimento("direita")
+# quebra_cabeca.tabuleiro.print_tabuleiro()
 # quebra_cabeca.final_state_verifier()
 quebra_cabeca.busca_em_largura()
--- quebra_cabeca/tabuleiro.py
+++ quebra_cabeca/tabuleiro.py
@@ -1,10 +1,21 @@
 import random 
 
+#
+#        X
+#     Y [2, 3, 4, 5]
+#       [1, 0, 2, 4]
+#
+#    Y = ALTURA
+#    X = HORIZONTAL
+
+
 class Tabuleiro:
-  def __init__(self, lado, random = True):
+  def __init__(self, lado, random = True, custom_state = []):
     self.lado = lado
-    if(random):
+    if(random and custom_state == []):
       self.incializa_tabuleiro_random()
+    elif(custom_state):
+       self.inicializa_tabuleiro_custom(custom_state)
     else: 
       self.inicializa_tabuleiro()
     self.movimentos = {'cima': self.mover_cima, 'baixo': self.mover_baixo, 'direita': self.mover_direita, 'esquerda': self.mover_esquerda}
@@ -15,29 +26,44 @@ class Tabuleiro:
     self.x = self.lado - 1
     self.y = self.lado - 1
     
+  def inicializa_tabuleiro_custom(self, custom_state):
+    self.tabuleiro = custom_state
+    player_position = self.get_player_position() #pega posição do player e seta no self.x e self.y
+    self.y = player_position[0]
+    self.x = player_position[1]
+    
   def incializa_tabuleiro_random(self):
     numbers = list(range(0, self.lado * self.lado)) # gerando os numeros
     random.shuffle(numbers) #numeros na ordem aleatoria
     self.tabuleiro = [[numbers.pop(0) if numbers else 0 for _ in range(self.lado)] for _ in range(self.lado)] #botando os elementos na matriz
     
     player_position = self.get_player_position() #pega posição do player e seta no self.x e self.y
-    self.x = player_position[0]
-    self.y = player_position[1]
+    self.y = player_position[0]
+    self.x = player_position[1]
     
   def get_player_position(self):
      for i in range(self.lado):
         for j in range(self.lado):
             if self.tabuleiro[i][j] == 0:
               return i, j
+            
+  def identificar_movimento(self, movimento):
+    if(movimento == "cima"):
+      self.mover_cima()
+    if(movimento == "baixo"):
+      self.mover_baixo()
+    if(movimento == "esquerda"):
+      self.mover_esquerda()
+    if(movimento == "direita"):
+      self.mover_direita()
 
   def troca(self, novo_x, novo_y):
-    aux = self.tabuleiro[novo_x][novo_y]
-    self.tabuleiro[novo_x][novo_y] = self.tabuleiro[self.x][self.y]
-    self.tabuleiro[self.x][self.y] = aux
+    aux = self.tabuleiro[novo_y][novo_x] # pegando o elemento na posição
+    self.tabuleiro[novo_y][novo_x] = self.tabuleiro[self.y][self.x]
+    self.tabuleiro[self.y][self.x] = aux
 
   def mover_cima(self):
     if self.pode_subir():
-        print(f'para cima: {self.x}{self.y-1}')
         self.troca(self.x, self.y - 1)
         self.y = self.y - 1
         return True
@@ -111,4 +137,4 @@ class Tabuleiro:
 
   def print_tabuleiro(self):
     for linha in self.tabuleiro:
-        print(linha)
+      print(linha)
