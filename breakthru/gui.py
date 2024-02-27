704409cc6113e35ad86cf70515a6df4e71261da4
PedroPortow <pedrolportow@gmail.com>
2024-02-27 20:18:20 -0300

    selected button color change

--- breakthru/gui.py
+++ breakthru/gui.py
@@ -24,25 +24,26 @@ class GUI(App):
         board_data = self.board.board
         for row_index, row in enumerate(board_data):
             for col_index, cell in enumerate(row):
-                button = Button(text=cell if cell else '-', background_color=self.get_cell_color(cell))
-                button.bind(on_press=lambda instance, x=row_index, y=col_index: self.button_pressed(x, y))
+                button = Button(text=cell if cell else '-', background_color=self.get_cell_color(cell, row_index, col_index))
+                button.bind(on_press=lambda instance, x=row_index, y=col_index: self.button_pressed(instance, x, y))
                 self.button_positions[(row_index, col_index)] = button
                 board_layout.add_widget(button)
 
         main_layout.add_widget(board_layout)
 
-        print(self.board.turn)
         self.turn_label = Label(text='', font_size=24, size_hint_x=None, width=200)
         self.set_turn_label()
         main_layout.add_widget(self.turn_label)
-
+        
         return main_layout
     
     def set_turn_label(self):
         self.turn_label.text = "Sua vez!" if self.board.is_player_turn else "Vez do robozão :("
 
-    def get_cell_color(self, cell):
-        if cell == 'S':
+    def get_cell_color(self, cell, row, col):
+        if self.first_button_pressed == (row, col):
+            return (0, 1, 0, 1)  
+        elif cell == 'S':
             return (0.5, 0.5, 0.5, 1)
         elif cell == 'G':
             return (1, 0.84, 0, 1)
@@ -51,20 +52,24 @@ class GUI(App):
         else:
             return (0.8, 0.8, 0.8, 1)
 
-    def button_pressed(self, row, col):
+    def button_pressed(self, instance, row, col):
+        # IF !INVALID POSITION... clicou em uma posição nada ve 
         if not self.first_button_pressed:
             self.first_button_pressed = (row, col)
+            self.update_button_colors()  
             return
 
-        if self.first_button_pressed == (row, col): # CANCELOU A JOGADA!
+        if self.first_button_pressed == (row, col):  # CANCELOU A JOGADA!
             self.first_button_pressed = None
+            self.update_button_colors()  # update as cores
             return
-    
-        # AQUI VAI MUDAR A VEZ DO JOGADOR
-        # depois da jogada
-        # self.set_turn_label()
+
 
         print(f"BUTTON ROW => {row}  col => {col} selected")
+
+    def update_button_colors(self):
+        for (row, col), button in self.button_positions.items():
+            button.background_color = self.get_cell_color(button.text, row, col)
 
 if __name__ == '__main__':
     GUI().run()
